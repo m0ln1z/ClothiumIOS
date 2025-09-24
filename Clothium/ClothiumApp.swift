9d37b6935324057aaf0141885f1de7c269dbb458
Artur <anartur476@gmail.com>
2025-09-24 16:53:54 +0500

    Initial Commit

---
+++ Clothium/ClothiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothiumApp.swift
+//  Clothium
+//
+//  Created by artur on 24.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClothiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
